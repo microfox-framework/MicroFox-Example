6fb186044e0909e96427bccca870a7684e7107f9
Mahdi <mah454@mail.moke.ir>
2025-06-05 09:56:15 +0330

    Fix code

--- microfox-mybatis/src/main/java/ir/moke/example/MyBatisRunner.java
+++ microfox-mybatis/src/main/java/ir/moke/example/MyBatisRunner.java
@@ -22,14 +22,12 @@ public class MyBatisRunner {
         Address a2 = new Address("Iran", "Golestan", "Gorgan", "Azadi", "951753464");
         Client client = new Client("Mahdi", "Sheikh Hosseini", List.of(a1, a2));
 
-        mybatis("h2", ClientMapper.class).save(client);
-        mybatis("h2", AddressMapper.class).save(a1, client);
-        mybatis("h2", AddressMapper.class).save(a2, client);
+        mybatis("h2", ClientMapper.class, clientMapper -> clientMapper.save(client));
+        mybatis("h2", AddressMapper.class, addressMapper -> addressMapper.save(a1, client));
+        mybatis("h2", AddressMapper.class, addressMapper -> addressMapper.save(a2, client));
 
-        List<Address> addressList = mybatis("h2", AddressMapper.class).findAddressByClientId(1);
-
-        for (Address address : addressList) {
-            System.out.println(JsonUtils.toJson(address));
-        }
+        mybatis("h2", AddressMapper.class, addressMapper -> {
+            addressMapper.findAll().forEach(item -> System.out.println(JsonUtils.toJson(item)));
+        });
     }
 }
