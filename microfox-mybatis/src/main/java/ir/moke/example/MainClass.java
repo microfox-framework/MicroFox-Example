f6f0ece461bcf1da53500fc52f8a755fc014f222
Mahdi <mah454@mail.moke.ir>
2025-06-07 20:49:59 +0330

    Fix code

--- microfox-mybatis/src/main/java/ir/moke/example/MainClass.java
+++ microfox-mybatis/src/main/java/ir/moke/example/MainClass.java
@@ -5,11 +5,11 @@ import ir.moke.example.persistence.mapper.AddressMapper;
 import ir.moke.example.persistence.mapper.ClientMapper;
 import ir.moke.example.persistence.model.Address;
 import ir.moke.example.persistence.model.Client;
-import ir.moke.microfox.utils.JsonUtils;
 
 import java.util.List;
 
 import static ir.moke.microfox.MicroFox.mybatis;
+import static ir.moke.microfox.MicroFox.mybatisTx;
 
 public class MainClass {
     static {
@@ -22,12 +22,11 @@ public class MainClass {
         Address a2 = new Address("Iran", "Golestan", "Gorgan", "Azadi", "951753464");
         Client client = new Client("Mahdi", "Sheikh Hosseini", List.of(a1, a2));
 
-        mybatis("h2", ClientMapper.class, clientMapper -> clientMapper.save(client));
-        mybatis("h2", AddressMapper.class, addressMapper -> addressMapper.save(a1, client));
-        mybatis("h2", AddressMapper.class, addressMapper -> addressMapper.save(a2, client));
+        mybatisTx("h2", ClientMapper.class, clientMapper -> clientMapper.save(client));
+        mybatisTx("h2", AddressMapper.class, addressMapper -> addressMapper.save(a1, client));
+        mybatisTx("h2", AddressMapper.class, addressMapper -> addressMapper.save(a2, client));
 
-        mybatis("h2", AddressMapper.class, addressMapper -> {
-            addressMapper.findAll().forEach(item -> System.out.println(JsonUtils.toJson(item)));
-        });
+        List<Address> list = mybatis("h2", AddressMapper.class, AddressMapper::findAll);
+        list.forEach(System.out::println);
     }
 }
